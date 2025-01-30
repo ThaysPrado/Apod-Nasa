1348215a0d952a60ddd29f6c0ab90111c64bda9c
Thays Ribeiro <thays.ribeiro@consulting-for.edenred.com>
2025-01-29 21:26:55 -0300

    new name

---
+++ APOD/APOD/APODApp.swift
@@ -0,0 +1,20 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Thays Prado on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ APOD/APOD/Extensions/FontExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  FontExtensions.swift
+//  Apod-Nasa
+//
+//  Created by Thays Prado on 29/01/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static func helvetica(with weight: NasaFontWeight, forTextStyle customStyle: NasaTextStyle) -> Font {
+        let name = "Helvetica-\(weight.rawValue)"
+        if let uiFont = UIFont(name: name, size: customStyle.size) {
+            return Font(uiFont as CTFont)
+        }
+        return .system(size: customStyle.size, weight: weight.swiftUIFontWeight, design: .default)
+    }
+}
+
